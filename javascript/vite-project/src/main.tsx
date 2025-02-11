e7c0f81e0580bcfd4e6c957b896d8b1953fb8da7
Brian Olson <beolson@hero4hire.com>
2025-02-11 09:25:44 -0600

    add 3rd party libraries

--- javascript/vite-project/src/main.tsx
+++ javascript/vite-project/src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router';
+
+import Flow from './Flow.tsx';
+import App from './App.tsx';
+import Dnd from './dnd/index.tsx';
+import Edit from './edit/index.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/Flow" element={<Flow />} />
+        <Route path="/Drag" element={<Dnd />} />
+        <Route path="/Edit" element={<Edit />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
