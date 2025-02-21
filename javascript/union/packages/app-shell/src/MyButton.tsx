7d9d082bf6671e762be633136d1c3592359a5bb2
Brian Olson <beolson@hero4hire.com>
2025-02-20 19:42:08 -0600

    update

--- javascript/union/packages/app-shell/src/MyButton.tsx
+++ javascript/union/packages/app-shell/src/MyButton.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
-export const MyButton = () => {
-  return <div>MyButton</div>;
+interface MyButtonProps {
+  label: string
+}
+export const MyButton = (props: MyButtonProps) => {
+  return <div>one {props.label}</div>;
 };
