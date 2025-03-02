1e10d4283185751959d642433e865e4762bfbe33
Brian Olson <beolson@hero4hire.com>
2025-03-02 13:28:34 -0600

    before install components

---
+++ javascript/union/packages/app-shell/install_components.sh
@@ -0,0 +1,7 @@
+
+pnpm dlx shadcn@latest add dropdown-menu
+pnpm dlx shadcn@latest add sidebar
+pnpm dlx shadcn@latest add avatar
+pnpm dlx shadcn@latest add collapsible
+pnpm dlx shadcn@latest add breadcrumb
+pnpm dlx shadcn@latest add separator
